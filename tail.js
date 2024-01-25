4bd24737026080c2063ac7b1d1d9d4a9269a268c
Thomas Azran <tommy.azran@gmail.com>
2024-01-25 16:12:34 +0000

    small AI corrective suggestions

--- tail.js
+++ tail.js
@@ -14,7 +14,9 @@ const tail = function(arr) {
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-const shapes = ["circle", "square", "triangle", "star"]
+const shapes = ["circle", "square", "triangle", "star"];
 assertEqual(tail(words).length, 2);
+assertEqual(tail(words)[0], "Lighthouse");
 assertEqual(words.length, 3);
 assertEqual(tail(shapes).length, 3);
+assertEqual(tail(shapes)[0], "square");
