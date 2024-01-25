b20a0408e285bf51a0cfc20974a29ad7a4c07529
Thomas Azran <tommy.azran@gmail.com>
2024-01-25 15:10:29 +0000

    final tail.js problem

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+};
+
+const tail = function(arr) {
+  if (arr.length <= 1) {
+    return [];
+  }
+  return arr.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
