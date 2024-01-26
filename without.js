36ecfaa81bfcdd0769ce29561655b88bc92711c2
Thomas Azran <tommy.azran@gmail.com>
2024-01-26 18:55:36 +0000

    without.js addition

---
+++ without.js
@@ -0,0 +1,33 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false; //these have to be false because they will only be false when the loop encounters something different
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; //these have to be false because they will only be false when the loop encounters something different
+    }
+  }
+  return true; //will always return true unless false --> will be incorrect if opposite
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑❌🛑 Assertion Failed: ${arr1} is not equal to ${arr2}!`);
+  } else {
+    console.log(`✅🟢✅ Assertion Passed: ${arr1} is equal to ${arr2}!`);
+  }
+};
+
+let wantedItems = [];
+const without = function(source, itemsToRemove){
+  for(let i = 0; i < source.length; i++){
+    if(!itemsToRemove.includes(source[i])){
+      wantedItems.push(source[i])
+    }
+  }
+  return wantedItems;
+};
+
+const candy = ["mars", "kitkat", "aero", "glossettes", "airheads", "rockets", "oh henry", "popeye"];
+console.log(without(candy, ["glossettes", "rockets", "mars"]));
+assertArraysEqual(candy, ["mars", "kitkat", "aero", "glossettes", "airheads", "rockets", "oh henry", "popeye"]);
