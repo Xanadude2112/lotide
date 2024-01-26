e6a756f13395061b147bc7cc390d1da0634df1c6
Thomas Azran <tommy.azran@gmail.com>
2024-01-26 20:10:04 +0000

    AI corrective suggestions

--- flatten.js
+++ flatten.js
@@ -22,9 +22,7 @@ const flatten = function(nestArr) {
   let oneDimensionArr = [];
   for (let i = 0; i < nestArr.length; i++) {
     if (Array.isArray(nestArr[i])) {
-      for (let j = 0; j < nestArr[i].length; j++) {
-        oneDimensionArr.push(nestArr[i][j]);
-      }
+      oneDimensionArr = oneDimensionArr.concat(nestArr[i]);
     } else {
       oneDimensionArr.push(nestArr[i]);
     }
