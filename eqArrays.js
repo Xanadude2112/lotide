48006fc2dade6a384b173c4953d6cd93f4a2bbcc
Thomas Azran <tommy.azran@gmail.com>
2024-01-26 17:13:07 +0000

    comments

--- eqArrays.js
+++ eqArrays.js
@@ -9,14 +9,14 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2){
   if(arr1.length !== arr2.length){
-    return false;
+    return false;     //these have to be false because they will only be false when the loop encounters something different
   }
 for(let i = 0; i < arr1.length; i++){
  if(arr1[i] !== arr2[i]){
-  return false
+  return false //these have to be false because they will only be false when the loop encounters something different
  }
 }
-return true
+return true //will always return true unless false --> will be incorrect if opposite
 };
 
 assertEqual(eqArrays(["animal"], ["animal"]), true);
