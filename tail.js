c5de51de520aff91ccc8dc769f9120ec2ad0d80e
Thomas Azran <tommy.azran@gmail.com>
2024-01-27 03:20:02 +0000

    tail.js additions

--- tail.js
+++ tail.js
@@ -1,3 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+  
+};
+
 const tail = function(arr) {
   if (arr.length <= 1) {
     return [];
@@ -7,8 +16,11 @@ const tail = function(arr) {
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const shapes = ["circle", "square", "triangle", "star"];
+const test = ["test"];
 assertEqual(tail(words).length, 2);
 assertEqual(tail(words)[0], "Lighthouse");
 assertEqual(words.length, 3);
 assertEqual(tail(shapes).length, 3);
 assertEqual(tail(shapes)[0], "square");
+assertEqual(tail(["test"]).length, 0); 
+assertEqual(tail([]).length, 0);
