0b3e888ff6d98597a21cf0e8599d28668539bdbb
Thomas Azran <tommy.azran@gmail.com>
2024-01-30 23:01:07 +0000

    countLetters.js solution

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+};
+
+const countLetters = (sentence) => {
+  let letCount = {};
+  for (let letter of sentence) {
+    if(letter === " "){
+      continue;
+    }else if(!letCount.hasOwnProperty(letter)){
+      letCount[letter] = 0;
+      letCount[letter] += 1;
+    } else {
+      letCount[letter] += 1;
+    }
+  }
+  console.log(letCount);
+  };
+
+  
+assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
+assertEqual(countLetters("coding is fun"), { c: 1, o: 2, d: 1, i: 2, n: 2, g: 1, s: 1, f: 1, u: 1});
