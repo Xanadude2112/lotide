6d408d9f755eb85b1d7412fb0e20ac4484f364e5
Thomas Azran <tommy.azran@gmail.com>
2024-01-30 23:10:51 +0000

    small corrections

--- countLetters.js
+++ countLetters.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  if (actual == expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} == ${expected}`);
     return;
   }
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected} `);
 };
 
 const countLetters = (sentence) => {
@@ -12,15 +12,14 @@ const countLetters = (sentence) => {
     if(letter === " "){
       continue;
     }else if(!letCount.hasOwnProperty(letter)){
-      letCount[letter] = 0;
-      letCount[letter] += 1;
+      letCount[letter] = 1;
     } else {
       letCount[letter] += 1;
     }
   }
-  console.log(letCount);
+  return letCount;
   };
 
-  
+
 assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
-assertEqual(countLetters("coding is fun"), { c: 1, o: 2, d: 1, i: 2, n: 2, g: 1, s: 1, f: 1, u: 1});
+assertEqual(countLetters("coding is fun"), { c: 1, o: 1, d: 1, i: 2, n: 2, g: 1, s: 1, f: 1, u: 1 });
