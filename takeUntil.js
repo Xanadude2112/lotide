93b18ec3c014925b9e839e976723d5ba24e73f89
Thomas Azran <tommy.azran@gmail.com>
2024-02-02 16:17:40 +0000

    initial commit of takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false; //these have to be false because they will only be false when the loop encounters something different
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; //these have to be false because they will only be false when the loop encounters something different
+    }
+  }
+  return true; //The function returns true if all elements in both arrays are equal, otherwise it returns false
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑❌🛑 Assertion Failed: ${arr1} is not equal to ${arr2}!`);
+  } else {
+    console.log(`✅🟢✅ Assertion Passed: ${arr1} is equal to ${arr2}!`);
+  }
+};
+
+const takeUntil = (array, callback) => {
+  let emptyArr = [];
+  for(let i = 0; i < array.length; i++){
+    if(!callback(array[i])){
+      emptyArr.push(array[i]);
+    }else{
+      break;
+    }
+  }
+  return emptyArr;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
