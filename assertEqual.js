9fcb44a0d21eb00a253568da916e1f6ca3235256
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 16:43:12 +0000

    small adjustments

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
+    return expected;
   }
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-  
+    return false;
 };
 
 module.exports = assertEqual;
