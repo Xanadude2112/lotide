8beedafd86c3f666f0c0a2f91fba9ddb1d24b741
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 00:42:11 +0000

    refactoring tail.js code

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  }
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-  
-};
-
 const tail = function(arr) {
   if (arr.length <= 1) {
     return [];
@@ -14,13 +5,4 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const shapes = ["circle", "square", "triangle", "star"];
-const test = ["test"];
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(words.length, 3);
-assertEqual(tail(shapes).length, 3);
-assertEqual(tail(shapes)[0], "square");
-assertEqual(tail(["test"]).length, 0); 
-assertEqual(tail([]).length, 0);
+module.exports = tail;
