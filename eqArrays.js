d354ebce8457e66e4fc7a2c443324085594c14c5
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 00:54:17 +0000

    refactor eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  }
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-  
-};
-
 const eqArrays = function(arr1, arr2){
   if(arr1.length !== arr2.length){
     return false;     //these have to be false because they will only be false when the loop encounters something different
@@ -19,7 +10,6 @@ for(let i = 0; i < arr1.length; i++){
 return true //will always return true unless false --> will be incorrect if opposite
 };
 
-assertEqual(eqArrays(["animal"], ["animal"]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
-assertEqual(eqArrays(["'1'", "'2'", "'3'"], [1, 2, 3, 4]), false);
+module.exports = eqArrays;
+
+
