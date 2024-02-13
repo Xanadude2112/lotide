d9db7b200bff38ef7809e6539d5d3f388dde7e44
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 01:08:51 +0000

    AI corrective suggestions on including eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false; //these have to be false because they will only be false when the loop encounters something different
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false; //these have to be false because they will only be false when the loop encounters something different
-    }
-  }
-  return true; //will always return true unless false --> will be incorrect if opposite
-};
-
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function (arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
     console.log(`🛑❌🛑 Assertion Failed: ${arr1} is not equal to ${arr2}!`);
