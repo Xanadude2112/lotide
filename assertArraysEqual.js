862a741138dfb9af072d75b3c277416af3862bf5
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 16:42:42 +0000

    smaall adjustments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,8 @@
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = function (arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
-    console.log(`🛑❌🛑 Assertion Failed: ${arr1} is not equal to ${arr2}!`);
     return false
   } else {
-    console.log(`✅🟢✅ Assertion Passed: ${arr1} is equal to ${arr2}!`);
     return true
   }
 };
