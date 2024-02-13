add31673c031234c311f4e9ddb6d80d8241d652b
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 16:35:41 +0000

    tailTest.js initialization of mocha chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,22 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
+const tail = require("../tail");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const shapes = ["circle", "square", "triangle", "star"];
-const test = ["test"];
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(words.length, 3);
-assertEqual(tail(shapes).length, 3);
-assertEqual(tail(shapes)[0], "square");
-assertEqual(tail(["test"]).length, 0); 
-assertEqual(tail([]).length, 0);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+  it("returns ['square', 'triangle', 'star'] for ['circle', 'square', 'triangle', 'star']", () => {
+    const shapes = ['circle', 'square', 'triangle', 'star'];
+    assert.deepEqual(tail(shapes), ['square', 'triangle', 'star']);
+  });
+  it("returns [] when given an array with 1 item or less within it", () => {
+    const test = ['test'];
+    assert.deepEqual(tail(test), []);
+  });
+  it("returns [] when given an array with 1 item or less within it", () => {
+    const gorp = [];
+    assert.deepEqual(tail(gorp), []);
+  });
+});
+
