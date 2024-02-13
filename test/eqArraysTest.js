0173ac902606b9c0de2fc7ee1345d6fa18fca8cf
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 16:34:46 +0000

    eqArraysTest.js initialization of mocha chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,20 @@
-const eqArrays = require('../eqArrays')
-const assertEqual = require('../assertEqual')
+const assert = require("chai").assert;
+const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays(["animal"], ["animal"]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "'3'"]), false);
-assertEqual(eqArrays(["'1'", "'2'", "'3'"], [1, 2, 3, 4]), false);
+describe("#eqArrays", () => {
+  it("returns true when ['animal'] is ['animal']", () => {
+    assert.strictEqual(eqArrays(['animal'], ['animal']), true)
+  });
+  it("returns true when [1,2,3] is [1,2,3]", () => {
+    assert.strictEqual(eqArrays([1,2,3], [1,2,3]), true)
+  });
+  it("returns false when [1,2,3] is [1,2,'3']", () => {
+    assert.strictEqual(eqArrays([1,2,3], [1,2,'3']), false)
+  });
+  it("returns false when ['1','2','3','4'] is [1,2,3,4]", () => {
+    assert.strictEqual(eqArrays(['1','2','3','4'], [1,2,3,4]), false)
+  });
+  it("returns false when ['1','2','3','4'] is ['1','2','3','4','5']", () => {
+    assert.strictEqual(eqArrays(['1','2','3','4'], ['1','2','3','4','5']), false)
+  });
+});
