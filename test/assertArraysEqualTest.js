415e885f8a51e04bbfd8a39532fc8da1e6f17ae3
Thomas Azran <tommy.azran@gmail.com>
2024-02-13 16:38:20 +0000

    assertArraysEqualTest.js initialization of mocha chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 2, "'3'"], [1, 2, "'3'"]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["triangle", "square", "circle"], ["triangle","square", "circle"]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+describe("#assertArraysEqual", () => {
+  it("returns true when [1, 2, '3'] is [1, 2, '3']", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, '3'], [1, 2, '3']), true);
+  });
+  it("returns false when [1, 2, '3'] is [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, '3'], [1, 2, 3]), false);
+  }); 
+  it("returns true when ['triangle', 'square', 'circle'], ['triangle','square', 'circle']", () => {
+    assert.strictEqual(assertArraysEqual(['triangle', 'square', 'circle'], ['triangle','square', 'circle']), true);
+  }); 
+});
